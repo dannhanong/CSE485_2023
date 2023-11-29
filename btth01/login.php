ad2a0b89d025949efdabfff1fcadf96651f51fe0
dannhanong <thannong0512@gmail.com>
2023-11-30 00:07:40 +0700

    login

--- btth01/login.php
+++ btth01/login.php
@@ -101,9 +101,5 @@
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <?php
-        $userName = $_POST['txtUser'];
-        echo $userName;
-    ?>
 </body>
 </html>
